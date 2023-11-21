c76c6d83e5d30459a3c383a0994803ca6ca44639
ketto26 <keto_nioradze@yahoo.com>
2023-11-21 17:49:59 +0400

    added package files

---
+++ Sources/NetworkManagerPackage/NetworkManagerPackage.swift
@@ -0,0 +1,40 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import Foundation
+
+func getData<T: Codable>(urlString: String, comletion: @escaping (Result<T,Error>) ->(Void)) {
+    let url = URL(string: urlString)!
+    
+    let session = URLSession()
+    
+    session.dataTask(with: URLRequest(url: url)) { data, response, error in
+        
+        if let error {
+            print(error.localizedDescription)
+        }
+        
+        guard let response = response as? HTTPURLResponse else {
+            return
+        }
+        
+        guard (200...299).contains(response.statusCode) else {
+            print("wrong response")
+            return
+        }
+        
+        guard let data else { return }
+
+        do {
+            let decoder = JSONDecoder()
+            let object = try decoder.decode(T.self, from: data)
+            
+            DispatchQueue.main.async {
+                comletion(.success(object))
+            }
+        } catch {
+            comletion(.failure(error))
+        }
+    }.resume()
+}
